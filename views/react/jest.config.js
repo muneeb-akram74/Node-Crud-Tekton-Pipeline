8e389f929358c2fe86be4954251b7c30336c5bbc
Andrew Shaw <ashaw85@hotmail.com>
2020-11-22 12:43:07 -0800

    communication-tools test can pass

---
+++ views/react/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+//  transform: {},
+  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "json"],
+  transform: {
+      '^.+\\.(js|jsx)?$': 'babel-jest'
+  },
+};
---
+++ views/react/src/communication-tools.test.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store'
+//import UserAndId from './billing';
+import Slate from './slate';
+import ErrorBoundary from './error-boundary';
+import 'bootstrap';
+//import "./billing.scss";
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+
+'use strict';
+
+//const Slate = jest.createMockFromModule('S');
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+})
+
+afterEach(() => {
+  //cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+})
+
+it("renders with ", () => {
+//  console.log('_globals:' + _globals);
+  act(() => {
+    render(
+      <ErrorBoundary>
+      <Provider store={store}>
+        <Slate 
+          message='Hi' 
+          messageMaxLength={4} 
+          readStatus='read' 
+          replyExists={true} 
+          getStateProperty={function(){}}
+          features={[1]}
+          backEndCaptchaEnabled={true}
+        />
+      </Provider>
+    </ErrorBoundary>, 
+    container);
+//    render(<NameInput />, container);
+  })
+  expect(container.textContent).toContain("mes");
+});
