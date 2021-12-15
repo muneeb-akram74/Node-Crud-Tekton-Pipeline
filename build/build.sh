6c9d20c9ddfe3e7159a6a347674f6f34225eca3a
unknown <hoochuanwu@yahoo.com>
2021-12-15 16:34:53 +0800

    master initialisation

---
+++ build/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "######################"
+echo Build Process
+echo "######################"
+pwd
+echo "P1 =  $1"
+# mkdir tmp
+read -p "Press [Enter] key to start build process..."
---
+++ deploy/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "######################"
+echo Deploy Process
+echo "######################"
+pwd
+echo "P1 =  $1"
+# mkdir tmp
+read -p "Press [Enter] key to start Deploy process..."
